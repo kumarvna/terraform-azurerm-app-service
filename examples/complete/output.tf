16abe5e037be4d6f7df8a6958ebe678fd7a6b2a0
kumarvna <36370570+kumarvna@users.noreply.github.com>
2021-07-24 15:19:07 +0530

    update module

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,72 +1,72 @@
 output "app_service_plan_id" {
   description = "The resource ID of the App Service Plan component"
-  value       = module.web-app.app_service_plan_id
+  value       = module.app-service.app_service_plan_id
 }
 
 output "maximum_number_of_workers" {
   description = " The maximum number of workers supported with the App Service Plan's sku"
-  value       = module.web-app.maximum_number_of_workers
+  value       = module.app-service.maximum_number_of_workers
 }
 
 output "app_service_id" {
   description = "The resource ID of the App Service component"
-  value       = module.web-app.app_service_id
+  value       = module.app-service.app_service_id
 }
 
 output "default_site_hostname" {
   description = "The Default Hostname associated with the App Service"
-  value       = module.web-app.default_site_hostname
+  value       = module.app-service.default_site_hostname
 }
 
 output "outbound_ip_addresses" {
   description = "A comma separated list of outbound IP addresses"
-  value       = module.web-app.outbound_ip_addresses
+  value       = module.app-service.outbound_ip_addresses
 }
 
 output "outbound_ip_address_list" {
   description = "A list of outbound IP addresses"
-  value       = module.web-app.outbound_ip_address_list
+  value       = module.app-service.outbound_ip_address_list
 }
 
 output "possible_outbound_ip_addresses" {
   description = "A comma separated list of outbound IP addresses - not all of which are necessarily in use. Superset of `outbound_ip_addresses`."
-  value       = module.web-app.possible_outbound_ip_addresses
+  value       = module.app-service.possible_outbound_ip_addresses
 }
 
 output "possible_outbound_ip_address_list" {
   description = "A list of outbound IP addresses - not all of which are necessarily in use. Superset of outbound_ip_address_list."
-  value       = module.web-app.possible_outbound_ip_address_list
+  value       = module.app-service.possible_outbound_ip_address_list
 }
 
 output "identity" {
   description = "An identity block, which contains the Managed Service Identity information for this App Service."
-  value       = module.web-app.identity
+  value       = module.app-service.identity
 }
 
 output "application_insights_id" {
   description = "The ID of the Application Insights component"
-  value       = module.web-app.application_insights_id
+  value       = module.app-service.application_insights_id
 }
 
 output "application_insights_app_id" {
   description = "The App ID associated with this Application Insights component"
-  value       = module.web-app.application_insights_app_id
+  value       = module.app-service.application_insights_app_id
 }
 
 output "application_insights_instrumentation_key" {
   description = "The Instrumentation Key for this Application Insights component"
-  value       = module.web-app.application_insights_instrumentation_key
+  value       = module.app-service.application_insights_instrumentation_key
   sensitive   = true
 }
 
 output "application_insights_connection_string" {
   description = "The Connection String for this Application Insights component"
-  value       = module.web-app.application_insights_connection_string
+  value       = module.app-service.application_insights_connection_string
   sensitive   = true
 }
 
 output "sas_url_query_string" {
-  value     = module.web-app.sas_url_query_string
+  value     = module.app-service.sas_url_query_string
   sensitive = true
 }
 
